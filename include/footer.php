a870219ac117e3713df25804aa74afb4e059e695
ajithcomorins <110983727+ajithcomorins@users.noreply.github.com>
2023-02-09 13:08:09 +0530

    footer page
    footer page upload

--- include/footer.php
+++ include/footer.php
@@ -1,12 +1,14 @@
-<footer class='container footer-section row'>
-    <div class='col-lg-6 col-md-12 text-sm-center text-md-center tex-lg-start text-center'>
-        <p>Copyright © 2023 Karka Acadamy</p>
-    </div>
-    <div class="col-lg-6 col-md-12 text-sm-center text-md-center text-lg-end text-center">
-        <a href="index.php" target="_self" class="text-decoration-none">Home</a>            
-        <a href="about.php" target="_self" class="text-decoration-none">About</a>            
-        <a href="fullstack.php" target="_self" class="text-decoration-none">Fullstack</a>            
-        <a href="fees_pap.php" target="_self" class="text-decoration-none">Service</a>            
-        <button type='button' class='get-in mt-3 sm-mt-0' onclick="window.open('https://karka.academy/apply/', '_blank')">GET TOUCH</button>        
+<footer class='container footer-section'>
+    <div class='row'>
+        <div class='col-lg-6 col-md-12 text-sm-center text-md-center text-lg-start text-center'>
+            <p>Copyright © 2023 Karka Acadamy</p>
+        </div>
+        <div class="col-lg-6 col-md-12 text-sm-center text-md-center text-lg-end text-center">
+            <a href="index.php" target="_self" class="text-decoration-none">Home</a>            
+            <a href="about.php" target="_self" class="text-decoration-none">About</a>            
+            <a href="fullstack.php" target="_self" class="text-decoration-none">Fullstack</a>            
+            <a href="fees_pap.php" target="_self" class="text-decoration-none">Service</a>            
+            <button type='button' class='get-in mt-3 sm-mt-0' onclick="window.open('https://karka.academy/apply/', '_blank')">GET TOUCH</button>        
+        </div>
     </div>
 </footer>
