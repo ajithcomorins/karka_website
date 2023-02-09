b5d1f6abf332402fa440285aaa5ea238a7d00031
ajithcomorins <110983727+ajithcomorins@users.noreply.github.com>
2023-02-09 15:18:34 +0530

    placement page
    placement page

---
+++ placement.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/karka-favicon-1.png">
+    <link rel="icon" type="image/png" href="assets/img/karka-favicon-1.png">
+    <title>Karka Academy | Placement Details</title>
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap" rel="stylesheet">
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <!-- CSS Files -->
+    <link id="pagestyle" href="assets/css/material-kit-pro.css?v=3.0.3" rel="stylesheet" />
+    <link id="pagestyle" href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+     <!-- header section start -->
+     <header class='about-us'>
+        <?php include 'include/header.php' ?>
+    </header>
+    <!-- heaader section end -->
+    <!-- section start  -->
+    <section class='placement' style="background-image: url('./assets/img/company-bg.webp')">
+      <div class='pt-5 pb-5 container container-85'>
+        <h2 class='text-center pb-5'>Karka Academy <br/>Placement Students</h2>
+        <div class='row m-0 align-items-center justify-content-center'>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Chaithanya">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Chaithanya</h3>
+                        <h4>Batch-9-2022</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+            <div class='col-lg-3 col-md-4 col-sm-6 col-12 pb-5'>
+                <div class="card" style="">
+                    <img src="./assets/img/aji.jpeg" class="card-img-top" alt="Ajith">
+                    <div class="card-body text-center">
+                        <h3 class="card-text">Ajith</h3>
+                        <h4>Batch-4-2020</h4>
+                        <img src='./assets/img/comorins.png' class='w-50'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <!-- company list view start -->
+      <div class='mb-4' style="background-image: url('./assets/img/company-bg.webp')">
+        <div class='container container-85 pb-6 pt-5'>
+          <div class="company_cover bg-white company-tab">
+            <h1 class="pt-6 mb-6 sm-mb-4 lg-mb-8 text-center  h1">Companies our students got placed</h1>    
+            <div class="row row-cols-lg-5 row-cols-md-3 row-cols-2 align-items-center justify-content-center px-104 company_tab_space">
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/Amazon-logo-1.png" alt="Amazon" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/Amadis.png" alt="Amadis" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/Amico.png" alt="Amico" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/Alo_Groups.png" alt="Colan-Infotech" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/comorins.png" alt="comorins" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/Colan-Infotech-Logo.png" alt="Colan-Infotech" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/DCS-Technology-logo.png" alt="DCS-Technology" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/emis-logo.png" alt="emis" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/flow-logo.png" alt="flow-logo" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/hcl-logo.png" alt="hcl" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/minerva-logo.png" alt="minerva" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/matrimony_logo.png" alt="" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/revyrie.png" alt="revyrie" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/tcs.png" alt="tcs" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/verticurl_logo_.png" alt="verticurl" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+              <div class="col p-16">
+                <img class="img-fluid " src="./assets/img/wibits_dwd.png" alt="wibits" loading="lazy" style="max-width: 100%; height: auto;">
+              </div>
+            </div> 
+          </div>
+        </div>
+      </div>
+      <!-- company list view end  -->
+    </section>
+    <!-- section end  -->
+    <!-- footer section start  -->
+    <?php include 'include/footer.php' ?>
+    <!-- footer section end  -->
+</body>
+    <!--   Core JS Files   -->
+    <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <!--  Plugin for TypedJS, full documentation here: https://github.com/mattboldt/typed.js/ -->
+    <script src="assets/js/plugins/typedjs.js"></script>
+    <script src="assets/js/plugins/choices.min.js"></script>
+    <script src="assets/js/plugins/flatpickr.min.js"></script>
+    <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+    <script src="assets/js/plugins/parallax.min.js"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+    <!--  Plugin for the blob animation -->
+    <script src="assets/js/plugins/anime.min.js" type="text/javascript"></script>
+    <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+    <script src="assets/js/material-kit-pro.min.js?v=3.0.3" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+
+</html>
